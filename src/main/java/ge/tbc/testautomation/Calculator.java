35273a185d3bcdd509ee9720cb517041f038e93a
Iamdachi04 <dachi4github@gmail.com>
2025-11-20 13:32:46 +0400

    Add division feature to the calculator

--- src/main/java/ge/tbc/testautomation/Calculator.java
+++ src/main/java/ge/tbc/testautomation/Calculator.java
@@ -2,6 +2,6 @@ package ge.tbc.testautomation;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Multiplication: 5 * 3 = 15");
+        System.out.println("Division: 6 / 3 = 2");
     }
 }
